e97aeec98b7fcc444a28ce828427637608d42543
sazalislam04 <mdsazalislam04@gmail.com>
2022-11-26 06:59:05 +0600

    added user delete function

--- src/Pages/Dashboard/Admin/AllUser.js
+++ src/Pages/Dashboard/Admin/AllUser.js
@@ -1,9 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import toast from "react-hot-toast";
+import Loading from "../../../Loading/Loading";
 
 const AllUser = () => {
-  const { data: users = [], refetch } = useQuery({
+  const {
+    data: users = [],
+    refetch,
+    isLoading,
+  } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
       const res = await fetch("http://localhost:5000/users");
@@ -26,6 +31,22 @@ const AllUser = () => {
       });
   };
 
+  const handleDelete = (id) => {
+    fetch(`http://localhost:5000/users/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.acknowledged) {
+          toast.success("User Deleted Successed");
+          refetch();
+        }
+      });
+  };
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <div>
       <h1 className="text-3xl">All Users</h1>
@@ -47,7 +68,7 @@ const AllUser = () => {
                 <td>{user.name}</td>
                 <td>{user.email}</td>
                 <td>
-                  {user?.role !== "admin" && (
+                  {user?.role !== "admin" ? (
                     <>
                       <button
                         onClick={() => handleMakeAdmin(user._id)}
@@ -56,10 +77,31 @@ const AllUser = () => {
                         Make Admin
                       </button>
                     </>
+                  ) : (
+                    <>
+                      <button disabled className="btn btn-success btn-sm">
+                        Admin
+                      </button>
+                    </>
                   )}
                 </td>
                 <td>
-                  <button className="btn btn-xs btn-warning">Delete</button>
+                  {user?.role !== "admin" ? (
+                    <>
+                      <button
+                        onClick={() => handleDelete(user._id)}
+                        className="btn btn-xs btn-warning"
+                      >
+                        Delete
+                      </button>
+                    </>
+                  ) : (
+                    <>
+                      <button disabled className="btn btn-xs btn-warning">
+                        Delete
+                      </button>
+                    </>
+                  )}
                 </td>
               </tr>
             ))}
